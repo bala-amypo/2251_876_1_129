0250843bef1d69a563dbf415127be1b3f41ed887
bala-amypo <bala@amypo.in>
2025-12-18 02:33:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/DiscountApplication.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+import java.math.BigDecimal;
+import java.security.Timestamp;
+import jakarta.persistence.*;
+
+public class DiscountApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private ManyToOne cart;
+    private ManyToOne bundleRule;
+    private BigDecimal discountAmount;
+    private Timestamp appliedAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public ManyToOne getCart() {
+        return cart;
+    }
+    public void setCart(ManyToOne cart) {
+        this.cart = cart;
+    }
+    public ManyToOne getBundleRule() {
+        return bundleRule;
+    }
+    public void setBundleRule(ManyToOne bundleRule) {
+        this.bundleRule = bundleRule;
+    }
+    public BigDecimal getDiscountAmount() {
+        return discountAmount;
+    }
+    public void setDiscountAmount(BigDecimal discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+    public Timestamp getAppliedAt() {
+        return appliedAt;
+    }
+    public void setAppliedAt(Timestamp appliedAt) {
+        this.appliedAt = appliedAt;
+    }
+    public DiscountApplication(ManyToOne cart, ManyToOne bundleRule, BigDecimal discountAmount, Timestamp appliedAt) {
+        this.cart = cart;
+        this.bundleRule = bundleRule;
+        this.discountAmount = discountAmount;
+        this.appliedAt = appliedAt;
+    }
+    public DiscountApplication() {
+    } 
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    public User() {
+    }
+}
