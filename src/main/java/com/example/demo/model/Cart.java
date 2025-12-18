83f461b07588b589d548a48fa8bedfd92a68b101
bala-amypo <bala@amypo.in>
2025-12-18 02:23:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Cart.java
@@ -0,0 +1,45 @@
+package com.example.demo.entity;
+
+import java.security.Timestamp;
+
+import jakarta.persistence.*;
+
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    public Cart(Long userId, Timestamp createdAt, Timestamp updatedAt) {
+        this.userId = userId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+    public Cart() {
+    }
+}
