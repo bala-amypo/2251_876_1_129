c84126c1e34b7043664cc131b4d63b2fe57fc1c1
bala-amypo <bala@amypo.in>
2025-12-23 04:26:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/CartItemController.java
+++ src/main/java/com/example/demo/controller/CartItemController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.CartItem;
-import com.example.demo.service.impl.CartItemServiceImpl;
+import com.example.demo.service.CartItemService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
